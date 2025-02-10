b5499b09f27e4080d8d8ef1e24a8be3191e4bf76
Eliza <elizapetrycka@gmail.com>
2025-02-10 14:03:12 +0100

    Added source, exe, dll files

---
+++ Eliza.Petrycka/source/Calculations.cs
@@ -0,0 +1,24 @@
+﻿namespace intern25
+{
+    public static class Calculations
+    {
+        public static double PerformCalculation(MathExpObject op)
+        {
+            try
+            {
+                return op.Oper switch
+                {
+                    "add" => (op.Value1 ?? 0) + (op.Value2 ?? 0),
+                    "sub" => (op.Value1 ?? 0) - (op.Value2 ?? 0),
+                    "mul" => (op.Value1 ?? 0) * (op.Value2 ?? 1),
+                    "sqrt" => Math.Sqrt(op.Value1 ?? 0),
+                    _ => throw new InvalidOperationException($"Nieznany operator: {op.Oper}")
+                };
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException(ex.Message);
+            }
+        }
+    }
+}
---
+++ Eliza.Petrycka/source/MathExpObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace intern25
+{
+    public class MathExpObject
+    {
+        [JsonPropertyName("operator")]
+        public string Oper { get; set; } = string.Empty;
+
+        [JsonPropertyName("value1")]
+        public double? Value1 { get; set; }
+
+        [JsonPropertyName("value2")]
+        public double? Value2 { get; set; }
+    }
+}
